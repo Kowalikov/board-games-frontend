80b28e133932082e74ec6f861cc575c7a998c332
Kowalikov <marekkowalik97@gmail.com>
2020-07-24 14:28:34 +0200

    Updates

---
+++ precache-manifest.17a6887022292024ee274b85fce42df7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f235e4dfdedf7447fbb0a67bcd5bcc",
+    "url": "/board-games-frontend/index.html"
+  },
+  {
+    "revision": "31fa3f9000abee0ad50e",
+    "url": "/board-games-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b616261792fd78dde40",
+    "url": "/board-games-frontend/static/js/2.c1d3c2f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/board-games-frontend/static/js/2.c1d3c2f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fa3f9000abee0ad50e",
+    "url": "/board-games-frontend/static/js/main.cc1168d5.chunk.js"
+  },
+  {
+    "revision": "1a9fa3795552e64a816a",
+    "url": "/board-games-frontend/static/js/runtime-main.f6eaf4ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/board-games-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
