b70611941f423bdd89980eb91ac00067c4531618
Kowalikov <marekkowalik97@gmail.com>
2020-07-29 13:01:40 +0200

    Updates

--- precache-manifest.2d61a2146bc4f3a1875b4e7c9735f378.js
+++ precache-manifest.2d61a2146bc4f3a1875b4e7c9735f378.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18a190b93e6b9aa2ebdd448b0b401813",
+    "revision": "99d8e3b576e27abfa368e5f17299e749",
     "url": "/board-games-frontend/index.html"
   },
   {
-    "revision": "69a3bccf0ae33a248a1f",
+    "revision": "69a53cfeb367533a01d8",
     "url": "/board-games-frontend/static/css/main.3a8fca12.chunk.css"
   },
   {
-    "revision": "0e1359d73014ef04a7b6",
-    "url": "/board-games-frontend/static/js/2.22a66064.chunk.js"
+    "revision": "a1f2c2ecc0339184cee7",
+    "url": "/board-games-frontend/static/js/2.c163f38d.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/board-games-frontend/static/js/2.22a66064.chunk.js.LICENSE.txt"
+    "url": "/board-games-frontend/static/js/2.c163f38d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "69a3bccf0ae33a248a1f",
-    "url": "/board-games-frontend/static/js/main.a8b8b92f.chunk.js"
+    "revision": "69a53cfeb367533a01d8",
+    "url": "/board-games-frontend/static/js/main.f285359d.chunk.js"
   },
   {
     "revision": "1a9fa3795552e64a816a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/board-games-frontend/precache-manifest.2e972da3be2848dd0ea388042e5b6fad.js"
+  "/board-games-frontend/precache-manifest.2d61a2146bc4f3a1875b4e7c9735f378.js"
 );
 
 self.addEventListener('message', (event) => {
