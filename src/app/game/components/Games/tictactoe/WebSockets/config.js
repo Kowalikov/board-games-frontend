b1075ae6d881fdeadaa46bd4866328173216d52e
Kowalikov <marekkowalik97@gmail.com>
2021-02-27 13:07:40 +0100

    updated API comment file

--- src/app/game/components/Games/tictactoe/WebSockets/config.js
+++ src/app/game/components/Games/tictactoe/WebSockets/config.js
@@ -1,2 +1,3 @@
 export const API_BASE = "https://kowalikov.github.io/board-games-frontend/"
+//export const API_BASE = "http://localhost:5000"
 export const WS_BASE = "wss://boardgames1.herokuapp.com/ws/match/"
